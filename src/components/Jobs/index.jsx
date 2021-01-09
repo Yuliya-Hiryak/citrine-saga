99db5d6bfd44fb657ef7db2d3cfbb9c2600250e7
Yuliya-Hiryak <yuliya.g@thescimus.com>
2021-01-09 13:40:22 +0200

    Add redux

--- src/components/Jobs/index.jsx
+++ src/components/Jobs/index.jsx
@@ -1,13 +1,25 @@
 import React, {useEffect} from 'react';
 import { JOBS } from 'modules/api/endpoints';
+import {useSelector, useDispatch} from 'react-redux';
+
 import api from 'modules/api/api';
+import { apiActions } from 'modules/api/actions';
 
 export default function Jobs() {
-    useEffect(async () => {
-        const response = await api.fetch(JOBS);
+    const state = useSelector(state => state.api[JOBS])
+    const dispatch = useDispatch();
 
-        console.log(response)
+    useEffect(async () => {
+        try{
+            dispatch(apiActions.fetch(JOBS))
+            const data = await api.fetch(JOBS)
+            dispatch(apiActions.fetchSuccess(JOBS, data))
+        } catch(e) {
+            dispatch(apiActions.fetchFailure(JOBS, e))
+        }
     }, []);
+    
+    console.log(state)
 
     return (
         <div>
